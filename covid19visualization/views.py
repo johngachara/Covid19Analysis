0f26bebe751f45213f52e7c792253b2e41cdd2a2
John Gachara <johngachara29@gmail.com>
2023-10-26 07:24:11 +0300

    second commit

--- covid19visualization/views.py
+++ covid19visualization/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from io import BytesIO
 import pandas as pd
-import base64
 import seaborn as sns
 import random
 import matplotlib.pyplot as plt
@@ -10,9 +8,23 @@ def home(request):
    dpath = '/home/gachara/PycharmProjects/djangoProject8/covid_19_clea n_complete.csv'
    data = pd.read_csv(dpath)
    num1 = random.randint(0,20)
-   num2 = random.randint(21,300)
+   num2 = random.randint(21,1000)
    data = data.iloc[num1:num2]
    data = data[["Country/Region","Date","Confirmed","Deaths","Recovered","Active"]]
    sns.pairplot(data,hue="Country/Region")
-   plt.savefig("static/charts/chart1.png")
+   plt.savefig("static/charts/photo1.jpg")
    return render(request,'home.html',{"data":data})
+def other(request):
+   path = '/home/gachara/PycharmProjects/djangoProject8/covid_19_clea n_complete.csv'
+   data = pd.read_csv(path)
+   data = data.head(3000)
+   comp = data['Confirmed']
+   death = data['Deaths']
+   recov = data['Recovered']
+   sns.barplot(data=data, x=death, y=comp)
+   plota = plt.savefig("static/page2/chart2.png")
+   sns.catplot(data,x=death,y=comp)
+   plotb = plt.savefig("static/page2/chart3.png")
+   sns.catplot(data,x=recov,y=death)
+   plotc = plt.savefig("static/page2/chart4.jpg")
+   return render(request,'other.html')
